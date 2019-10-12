8ace42065e16d96420c6a90b138717794c79995d
rithvikg <rithvikg123@gmail.com>
2019-10-12 01:07:49 -0700

    Reformatting repo

---
+++ string-calculator/Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("String Calculator\n");
+			Console.WriteLine("Please enter input\n");
+			var strInput = Console.ReadLine();
+
+			Calcultor calc = new Calcultor(strInput);
+
+			calc.CalculateTwoNumbers();
+
+
+		}
+	}
+}
