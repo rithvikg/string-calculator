f0a9f4931a3ec4aab1d58fdd170ab6ccf9fefd73
rithvikg <rithvikg@users.noreply.github.com>
2019-10-14 00:08:33 -0700

    Gui Spacing Issues

--- string-calculator/Calculator/Calculator.cs
+++ string-calculator/Calculator/Calculator.cs
@@ -30,74 +30,72 @@ namespace string_calculator
 		{
 			int result = 0;
 			string calcInput = GetInput();
-            List<int> negativeNumbers = new List<int>(); //List of negative numbers to return
-            List<String> delimiterList = new List<String>(); //List of delimeters to split Input
+            		List<int> negativeNumbers = new List<int>(); //List of negative numbers to return
+            		List<String> delimiterList = new List<String>(); //List of delimeters to split Input
 
-            delimiterList.Add(",");
-            delimiterList.Add("\\n");
+            		delimiterList.Add(",");
+            		delimiterList.Add("\\n");
 
             
-            string[] delimiterSplit = //Used to split custom Delimiter of any length
-            {
-                "[",
-                "]",
+            		string[] delimiterSplit = //Used to split custom Delimiter of any length
+            		{
+                		"[",
+                		"]",
                 
-            };
+            		};
             
-            var singleDelimiter = Regex.Match(calcInput, @"//(.)\\n\d").Groups[1].Value;
-            var customDelimiterCheck = Regex.Match(calcInput, @"//(\[(.*)\])+\\n\d").Groups[1].Value;
+            		var singleDelimiter = Regex.Match(calcInput, @"//(.)\\n\d").Groups[1].Value;
+            		var customDelimiterCheck = Regex.Match(calcInput, @"//(\[(.*)\])+\\n\d").Groups[1].Value;
 
-            var customDelimiter = customDelimiterCheck.Split(delimiterSplit, StringSplitOptions.RemoveEmptyEntries);
+            		var customDelimiter = customDelimiterCheck.Split(delimiterSplit, StringSplitOptions.RemoveEmptyEntries);
 
-            if (singleDelimiter.Length > 0)
-            {
-                delimiterList.Add(singleDelimiter);
-            }
+            		if (singleDelimiter.Length > 0)
+            		{
+                		delimiterList.Add(singleDelimiter);
+            		}
 
-            if (customDelimiter.Length > 0)
-            {
-                for (int i = 0; i < customDelimiter.Length; i++)
-                {
-                    delimiterList.Add(customDelimiter[i]);
-                }
-            }
+            		if (customDelimiter.Length > 0)
+            		{
+                		for (int i = 0; i < customDelimiter.Length; i++)
+                		{
+                    			delimiterList.Add(customDelimiter[i]);
+                		}
+            		}
 
-            string[] delimiterChars = delimiterList.ToArray();
+            		string[] delimiterChars = delimiterList.ToArray();
 
-            var numList = calcInput.Split(delimiterChars, StringSplitOptions.None); //To only get numbers
+            		var numList = calcInput.Split(delimiterChars, StringSplitOptions.None); //To only get numbers
             
-            for (int i = 0; i < numList.Length; i++)
+            		for (int i = 0; i < numList.Length; i++)
 			{
 				int.TryParse(numList[i], out int n);
 
-                if (n < 0)
-                {
-                    negativeNumbers.Add(n);
-                }
-                else if (n >= 1000)
-                {
-                    stretchResult += 0;
-                    
-                }
-                else
-                {
-                    result += n;
-                    stretchResult += n;
-                    
-                }
-                if (i != numList.Length - 1)
-                {
-                    stretchResult += "+";
-                }
-            }
-            if (negativeNumbers.Count > 0)
-            {
-                string negNum = string.Join(",", negativeNumbers.ToArray()); //Converting list to string to print in exception
-                throw new Exception("Negative Numbers Not Allowed. Negative Numbers Inputted: " + negNum);
-            }
-            Console.WriteLine(stretchResult + " = " + result);
-            return result;
+                		if (n < 0)
+                		{
+                    			negativeNumbers.Add(n);
+                		}
+                		else if (n >= 1000)
+                		{
+                    			stretchResult += 0;                    
+                		}
+                		else
+                		{
+                    			result += n;
+                    			stretchResult += n;                   
+                		}
+                		if (i != numList.Length - 1)
+                		{
+                    			stretchResult += "+";
+                		}
+           	 	}
+            		if (negativeNumbers.Count > 0)
+            		{
+                		string negNum = string.Join(",", negativeNumbers.ToArray()); //Converting list to string to print in exception
+                		throw new Exception("Negative Numbers Not Allowed. Negative Numbers Inputted: " + negNum);
+            		}
+            		Console.WriteLine(stretchResult + " = " + result);
+            		return result;
 			                      
-        }
-    }
+        	}
+	}
 }
